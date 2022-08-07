aabf425de69de6ce08a47d7f93f0360407fc69c5
Steven Conaway <steven.conaway@icloud.com>
2022-08-06 19:18:33 -0700

    move /board to /

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 const {App} = require('@slack/bolt');
 const getPixels = require('get-pixels');
-// const path = require('path');
-// const fs = require('fs');
+const path = require('path');
+const fs = require('fs');
 
 require('dotenv').config();
 const sharp = require('sharp');
+
+const colors = {
+    PrismaticVermilionCardinalRenewalAirbnbAlabamaRed: [202, 0, 33],
+    NormalRed: [225, 0, 33],
+    EvenMoreNormalRed: [225, 0, 0],
+    CelesteTheColor: [178, 255, 255],
+    CelesteThePerson: [178, 255, 255],
+    Periwinkle: [178, 255, 255],
+    Bluheheh: [255, 0, 0],
+    IfYourUrineIsThisColorSeeADoctor: [57, 255, 20],
+    GreenishGreen: [0, 255, 0],
+    PakistanGreen: [0, 102, 0],
+    Purpureus: [154, 78, 174],
+    RedAndBlueButNotPurple: [255, 0, 255],
+    DrinkMoreWaterPiss: [218, 165, 32],
+    StillDrinkMoreWaterPiss: [253, 255, 0],
+    GoodJobYouDrankEnoughWaterPiss: [255, 255, 224],
+    ItsJustOrange: [240, 97, 5],
+};
 
 const token = process.env.TOKEN;
 const signingSecret = process.env.SIGNING_SECRET;
 
 const maxRows = 26;
 const maxCols = 11;
-
+let flashtime = 0;
+let flash;
 const ratelimit = true;
 
-const helpText = `Please enter a cell and color in the form of /chaosgrid <row> <col> <r> <g> <b>\n
+const helpText = `Please enter a cell and color in the form of \`/chaosgrid <row> <col> <r> <g> <b>\`\n
     Other Commands:\n
-    /chaosflashbang (fun)`;
+    \`/chaosflashbang\` (fun)`;
 
 let recents = [];
 let board = [];
@@ -49,7 +69,7 @@ function randomcolor() {
     return rgb2emoji(
         Math.min(Math.floor(Math.random() * 255) + 200, 255),
         Math.min(Math.floor(Math.random() * 255) + 200, 255),
-        Math.min(Math.floor(Math.random() * 255) + 200, 255),
+        Math.min(Math.floor(Math.random() * 255) + 300, 255),
     );
 }
 
@@ -91,18 +111,18 @@ function initBoard() {
 }
 
 function createFlash() {
-    console.log('got pixels', pixels.shape.slice());
-    board = [];
+    flash = [];
 
     for (let i = 0; i < 26; i++) {
-        board.push([]);
+        flash.push([]);
         for (let j = 0; j < 11; j++) {
-            board[i].push(randomcolor());
+            flash[i].push(randomcolor());
         }
     }
 }
 
 initBoard();
+createFlash();
 
 const app = new App({
     signingSecret,
@@ -116,36 +136,6 @@ const app = new App({
                     res.setHeader('Content-Type', 'text/html');
                     // damn, good one!
                     fs.createReadStream('public/index.html').pipe(res, {
-                        end: true,
-                    });
-                } catch (err) {
-                    console.error(err);
-                }
-            },
-        },
-        {
-            path: '/index2',
-            method: ['GET'],
-            handler(req, res) {
-                try {
-                    res.setHeader('Content-Type', 'text/html');
-                    // damn, good one!
-                    fs.createReadStream('public/index2.html').pipe(res, {
-                        end: true,
-                    });
-                } catch (err) {
-                    console.error(err);
-                }
-            },
-        },
-        {
-            path: '/board',
-            method: ['GET'],
-            handler(req, res) {
-                try {
-                    res.setHeader('Content-Type', 'text/html');
-                    // damn, good one!
-                    fs.createReadStream('public/board.html').pipe(res, {
                         end: true,
                     });
                 } catch (err) {
@@ -268,7 +258,7 @@ function createTitle() {
     return title;
 }
 
-function createMessage() {
+function createMessage(board) {
     let msg = '      A    B    C    D    E    F     G    H    I      J     K';
 
     for (let i = 0; i < 26; i++)
@@ -279,7 +269,7 @@ function createMessage() {
 
 function updaterec() {
     for (let i = 0; i < recents.length; i++) {
-        if (new Date() - recents[i][1] >= 60000) {
+        if (new Date() - recents[i][1] >= 29999) {
             recents.splice(i, 1);
             i--;
             console.log(recents);
@@ -302,13 +292,13 @@ function updaterec() {
 
         let d = new Date();
         if (body.text == '') {
-            await respond(`${createTitle()} \n${createMessage()}`);
+            await respond(`${createTitle()} \n${createMessage(board)}`);
             await respond({text: helpText, response_type: 'ephemeral'});
             return;
         }
         if (ratelimit && recents.some((recent) => recent[0] === body.user_id)) {
             await respond({
-                text: 'You can only send a message once every 59.9999999 seconds',
+                text: 'You can only send a message once every 29.9999999 seconds',
                 response_type: 'ephemeral',
             });
             return;
@@ -317,7 +307,7 @@ function updaterec() {
         recents.push([body.user_id, d]);
 
         try {
-            parsemsg(body.text);
+            await parsemsg(body.text, respond);
         } catch (error) {
             await respond({
                 text: `Zoinks, something went wrong: ${error}.\n\n${helpText}`,
@@ -326,13 +316,19 @@ function updaterec() {
             return;
         }
 
-        await say(`${createTitle()} \n${createMessage()}`);
+        await say(`${createTitle()} \n${createMessage(board)}`);
     });
 
-    app.command('/chaoshelp', async ({body, ack, say}) => {
+    app.command('/chaoshelp', async ({ack, respond}) => {
         await ack();
 
         await respond({text: helpText, response_type: 'ephemeral'});
+    });
+
+    app.command('/chaosflashbang', async ({body, ack, say}) => {
+        await ack();
+        flashtime = 5;
+        await say(createMessage(flash) + '\n-FLASHBANG- (your eyes bleed)');
     });
 
     app.message('chaos', async ({message, say, ack}) => {
